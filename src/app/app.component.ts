d84570dd223191636c9c70b6e96fd35383937e90
Kermo880 <kermo.henn@vikk.ee>
2020-11-30 15:55:42 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  constructor(http: HttpClient) {
+    http
+      .get("https://en.wikipedia.org/api/rest_v1/page/summary/Viljandi_County")
+      .subscribe(data => {
+        console.log(data);
+        this.pageSummary = data.extract;
+      });
+  }
+  name = "Angular " + VERSION.major;
+
+  pageSummary: string;
 }
