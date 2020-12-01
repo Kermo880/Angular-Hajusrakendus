9fa542c3befeda2f0fa85621ad4ba7bb1aba814e
Kermo880 <kermo.henn@vikk.ee>
2020-12-01 10:23:57 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, VERSION } from "@angular/core";
+import { Component, ElementRef, VERSION, ViewChild } from "@angular/core";
 
 @Component({
   selector: "my-app",
@@ -7,17 +7,31 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  constructor(http: HttpClient) {
-    http
+  @ViewChild("search") searchInput: ElementRef<HTMLInputElement>;
+
+  constructor(private http: HttpClient) {}
+
+  startSearch() {
+    console.log(this.searchInput.nativeElement.value);
+    this.http
       .get<any>(
-        "https://en.wikipedia.org/api/rest_v1/page/summary/Viljandi_County"
+        `https://en.wikipedia.org/api/rest_v1/page/summary/${
+          this.searchInput.nativeElement.value
+        }`
       )
-      .subscribe(data => {
-        console.log(data);
-        this.pageSummary = data.extract;
-      });
+      .subscribe(
+        data => {
+          console.log(data);
+          this.pageSummary = data.extract;
+          this.pageThumbnail = data.thumbnail.source;
+          this.name = data.titles.display;
+        },
+        error => {}
+      );
   }
-  name = "Angular " + VERSION.major;
+
+  name: string;
 
   pageSummary: string;
+  pageThumbnail: string;
 }
